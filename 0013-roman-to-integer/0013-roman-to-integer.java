6b62009aa843cae54acfa249d19f597be9a9fdca
Dinesh Prasad <88045947+Dinesh9928@users.noreply.github.com>
2024-02-12 11:10:57 +0530

    Time: 6 ms (37.89%), Space: 44.7 MB (56.51%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> rom = new HashMap<>();
+        rom.put('I', 1);
+        rom.put('V', 5);
+        rom.put('X', 10);
+        rom.put('L', 50);
+        rom.put('C', 100);
+        rom.put('D', 500);
+        rom.put('M', 1000);
+
+        s = s.replace("IV", "IIII").replace("IX", "VIIII");
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX");
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC");
+
+        int sum = 0;
+        for (char c : s.toCharArray()) {
+            sum += rom.get(c);
+        }
+
+        return sum;
+    }
+}
